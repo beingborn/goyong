daea32e4c1059dd0712bcec5ba5fd536cfab2f42
beingborn <dolmin0708@naver.com>
2024-07-22 17:43:12 +0900

    first commit in IT PUB

---
+++ js/default.js
@@ -0,0 +1,29 @@
+
+
+
+var swiper = new Swiper(".bannerSwiper", {
+    slidesPerView : 'auto',
+    spaceBetween: 32,
+    loop: true,
+    loopAdditionalSlides : 1,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".mainSwiper", {
+    slidesPerView : 'auto',
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    on: {
+    activeIndexChange : function() {
+        
+    },
+    },
+  });
