404cb143c7a44fae05b71b543ba7d59fe61bcd8a
beingborn <dolmin0708@naver.com>
2024-08-06 11:17:08 +0900

    link 연결 전

--- js/manage__menu.js
+++ js/manage__menu.js
@@ -51,15 +51,123 @@ $(document).ready(function(){
     $('.menu__depth-2').click(function(e){
         e.preventDefault(); // 링크 기본 동작 방지
 
-
+        let currentIndex = $(this).index();
         $(this).addClass("active");
         $('.menu__depth-2').not($(this)).removeClass('active')
-
-
+        $('.auth__wrap').hide().eq(currentIndex).show();
     })
 
     $('.menu__depth-2').eq(0).addClass('active');
     $('.auth__wrap').hide().eq(0).show();
 })
+
+
+/**
+ * 콘텐츠간 높이 구하기 위해 
+
+        부모 wrap scrollHeight 값에서 해당 엘리먼트 값 간의 차이 구해서 
+        오른쪽 radio wrap들에게 동적으로 정확한 값 주기 // radio__wrap은 position absolute 처리되어있음
+        만약 depth2의 offsetTop을 뺄경우에는 - 2 값을 해주어야한다.
+
+ * 
+ * 
+ */
+
+
+
+    // 임시
+
+    $(document).ready(function(){
+        for (i = 0; i <= 21; i++) {
+            if(i % 2 == 0 ){
+                $('.manager__auth .radio__wrap input[type="radio"]').eq(i).first().prop('checked', true)
+            }
+        }
+
+    })
+
+
+
+    let manageAuthScroll = $('.manager__auth').offset().top;
+
+    // 메뉴들을 배열로 정리
+    let menus = [
+    $('.manager__auth .depth__1__tree').eq(0).offset().top,
+    $('.manager__auth .depth__2__tree').eq(0).offset().top,
+    $('.manager__auth .depth__2__tree').eq(1).offset().top,
+    $('.manager__auth .depth__2__tree').eq(2).offset().top,
+    $('.manager__auth .depth__2__tree').eq(3).offset().top,
+    $('.manager__auth .depth__1__tree').eq(1).offset().top,
+    $('.manager__auth .depth__2__tree').eq(4).offset().top,
+    $('.manager__auth .depth__2__tree').eq(5).offset().top,
+    $('.manager__auth .depth__1__tree').eq(2).offset().top,
+    $('.manager__auth .depth__2__tree').eq(6).offset().top,
+    $('.manager__auth .depth__2__tree').eq(7).offset().top
+    ];
+
+
+
+    menus.forEach((menuOffsetTop, index) => {
+    let adjustment = 0;
+
+    // depth__2__tree일 경우 -2 값을 적용
+    if ($('.manager__auth .depth__2__tree').is(`:eq(${index})`)) {
+        adjustment = -2;
+    }
+
+    $('.manager__auth .radio__wrap').eq(index).css('margin-top', (menuOffsetTop - manageAuthScroll + adjustment));
+});
+
+
+
+
+
+$(document).ready(function(){
+    for (i = 0; i <= 21; i++) {
+        if(i % 2 == 0 ){
+            $('.union__auth .radio__wrap input[type="radio"]').eq(i).first().prop('checked', true)
+        }
+    }
+
+})
+
+
+
+let unionAuthScroll = $('.union__auth').offset().top;
+
+// 메뉴들을 배열로 정리
+let unionMenus = [
+$('.union__auth .depth__1__tree').eq(0).offset().top,
+$('.union__auth .depth__2__tree').eq(0).offset().top,
+$('.union__auth .depth__2__tree').eq(1).offset().top,
+$('.union__auth .depth__2__tree').eq(2).offset().top,
+$('.union__auth .depth__2__tree').eq(3).offset().top,
+$('.union__auth .depth__1__tree').eq(1).offset().top,
+$('.union__auth .depth__2__tree').eq(4).offset().top,
+$('.union__auth .depth__2__tree').eq(5).offset().top,
+$('.union__auth .depth__1__tree').eq(2).offset().top,
+$('.union__auth .depth__2__tree').eq(6).offset().top,
+$('.union__auth .depth__2__tree').eq(7).offset().top
+];
+
+
+
+unionMenus.forEach((menuOffsetTop, index) => {
+let adjustment = 0;
+
+// depth__2__tree일 경우 -2 값을 적용
+if ($('.union__auth .depth__2__tree').is(`:eq(${index})`)) {
+    adjustment = -2;
+}
+
+$('.union__auth .radio__wrap').eq(index).css('margin-top', (menuOffsetTop - unionAuthScroll + adjustment));
+});
+
+
+
+
+
+
+
 
 
